108858989cf92b6a99604e289d3bc8297174b9fe
jangco97 <qkrnslehd@naver.com>
2024-01-14 20:42:12 +0900

    chore: adminMain페이지

---
+++ my-app/src/pages/AdminMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AdminMain = () => {
+  return (
+    <Container>
+      <h1>AdminMain</h1>
+    </Container>
+  );
+};
+
+export default AdminMain;
+
+const Container = styled.div`
+  padding: 20px;
+`;
