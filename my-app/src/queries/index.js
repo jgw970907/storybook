462530238a1735fbf0da4efe9e4a9da52c7738b0
jangco97 <qkrnslehd@naver.com>
2024-01-17 23:36:36 +0900

    feat: 댓글 react-query 요청

--- my-app/src/queries/index.js
+++ my-app/src/queries/index.js
@@ -1,20 +1,26 @@
-import { useQuery, useMutation, useQueryClient } from 'react-query';
-import queryKeys from 'queries/queryKeys';
-import { getBoard, getBoards, createBoard, patchBoard, deleteBoard } from '../api/boardApi';
-import { getBoardComments } from '../api/replyApi';
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import queryKeys from "queries/queryKeys";
+import {
+  getBoard,
+  getBoards,
+  createBoard,
+  patchBoard,
+  deleteBoard,
+} from "../api/boardApi";
+import { getBoardComments, createBoardComment } from "../api/replyApi";
 export const useGetBoard = (id) => {
   const { data, error, isLoading } = useQuery({
-    queryKey: [queryKeys.ADMIN, 'board'],
+    queryKey: [queryKeys.ADMIN, "board", `${id}`],
     queryFn: () => getBoard(id),
     select: (res) => res.data,
   });
   return { data, error, isLoading };
 };
 
-export const useGetBoards = () => {
+export const useGetBoards = ({ take, page, order, search }) => {
   return useQuery({
-    queryKey: [queryKeys.ADMIN, 'boards'],
-    queryFn: getBoards,
+    queryKey: [queryKeys.ADMIN, "boards"],
+    queryFn: () => getBoards(take, page, order, search),
     select: (res) => res.data,
   });
 };
@@ -23,10 +29,15 @@ export const useCreateBoard = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    queryKey: [queryKeys.ADMIN, 'boards'],
+    queryKey: [queryKeys.ADMIN, "board"],
     mutationFn: createBoard,
-    onSuccess: async () => {
-      await queryClient.invalidateQueries([queryKeys.ADMIN, 'boards']);
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries([queryKeys.ADMIN, "boards"]);
+      await queryClient.invalidateQueries([
+        queryKeys.ADMIN,
+        "board",
+        `${data?.id}`,
+      ]);
     },
   });
 };
@@ -35,11 +46,15 @@ export const usePatchBoard = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    queryKey: [queryKeys.ADMIN, 'boards'],
-    mutationFn: patchBoard,
+    queryKey: [queryKeys.ADMIN, "board"],
+    mutationFn: (id, board) => patchBoard(id, board),
     onSuccess: async (data) => {
-      await queryClient.invalidateQueries([queryKeys.ADMIN, 'boards']);
-      await queryClient.invalidateQueries([queryKeys.ADMIN, 'board', `${data?.id}`]);
+      await queryClient.invalidateQueries([queryKeys.ADMIN, "boards"]);
+      await queryClient.invalidateQueries([
+        queryKeys.ADMIN,
+        "board",
+        `${data?.id}`,
+      ]);
     },
   });
 };
@@ -48,19 +63,40 @@ export const useDeleteBoard = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    queryKey: [queryKeys.ADMIN, 'boards'],
+    queryKey: [queryKeys.ADMIN, "boards"],
     mutationFn: deleteBoard,
     onSuccess: async (data) => {
-      await queryClient.invalidateQueries([queryKeys.ADMIN, 'boards']);
-      await queryClient.invalidateQueries([queryKeys.ADMIN, 'board', `${data?.id}`]);
+      await queryClient.invalidateQueries([queryKeys.ADMIN, "boards"]);
+      await queryClient.invalidateQueries([
+        queryKeys.ADMIN,
+        "board",
+        `${data?.id}`,
+      ]);
     },
   });
 };
 
 export const useGetBoardComments = (boardId) => {
   return useQuery({
-    queryKey: [queryKeys.ADMIN, 'BoardComments', `${boardId}`],
+    queryKey: [queryKeys.USER, "BoardComments", `${boardId}`],
     queryFn: () => getBoardComments(boardId),
     select: (res) => res.data,
+  });
+};
+
+export const useCreateBoardComment = (boardId) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    queryKey: [queryKeys.USER, "BoardComments", `${boardId}`],
+    mutationFn: (commentData) =>
+      createBoardComment(commentData.boardId, commentData.comment),
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries([
+        queryKeys.USER,
+        "BoardComments",
+        `${boardId}`,
+      ]);
+    },
   });
 };
