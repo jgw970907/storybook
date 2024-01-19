a8cd00374bdda052af0551aa755e67fc680f3233
jangco97 <qkrnslehd@naver.com>
2024-01-19 23:21:54 +0900

    feat: 상세페이지 검색기능과 페이지네이션 구축

--- my-app/src/pages/BoardDetail.jsx
+++ my-app/src/pages/BoardDetail.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import { useGetBoard } from 'pages/admin/dev5/queries';
-import { useGetBoardComments } from 'pages/admin/dev5/queries';
-import Header from '../components/detailpageComponents/Header';
-import Body from '../components/detailpageComponents/Body';
-import Comment from '../components/detailpageComponents/Comment';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useUserStore } from "store/useUserStore";
+import secureLocalStorage from "react-secure-storage";
+
+import { useGetBoard } from "pages/admin/dev5/queries";
+import { useGetBoardComments } from "pages/admin/dev5/queries";
+import Header from "../components/detailpageComponents/Header";
+import Body from "../components/detailpageComponents/Body";
+import Comment from "../components/detailpageComponents/Comment";
+import CommentWrite from "../components/detailpageComponents/CommentWrite";
+import DeleteOrUpdateButton from "../components/detailpageComponents/DeleteOrUpdateButton";
 
 const BoardDetail = () => {
   const { boardId } = useParams();
   console.log(boardId);
-  const { isLoading: boardIsLoading, data: board, error: boardError } = useGetBoard(boardId);
+  const {
+    isLoading: boardIsLoading,
+    data: board,
+    error: boardError,
+  } = useGetBoard(boardId);
   console.log(board);
   const {
     isLoading: commentsIsLoading,
     data: comments,
     error: commentsError,
   } = useGetBoardComments(boardId);
+  const [isAdmin, setIsAdmin] = useState(false);
 
+  useEffect(() => {
+    const user = secureLocalStorage.getItem("USER_INFO");
+    console.log(user);
+
+    if (user) {
+      const { email } = JSON.parse(user);
+      if (email === board?.author.email) {
+        setIsAdmin(true);
+      }
+    }
+  }, [boardId]);
   if (boardIsLoading || commentsIsLoading) {
     return <div>Loading...</div>;
   }
@@ -30,22 +51,57 @@ const BoardDetail = () => {
   if (boardError || commentsError) {
     return <div>Error...</div>;
   }
+  if (comments) {
+    console.log(comments);
+  }
+
   return (
     <div>
-      <>
+      <S.container>
+        <S.titleContainer>
+          <S.title>제목: {board?.title}</S.title>
+          <S.deleteOrUpdateButton>
+            <DeleteOrUpdateButton isAdmin={isAdmin} />
+          </S.deleteOrUpdateButton>
+        </S.titleContainer>
+
         <Header
-          title={board?.title}
           email={board?.author.email}
           nickname={board?.author.nickname}
           likeCount={board?.likeCount}
+          replyCount={board?.reply5Count}
           createdAt={board?.createdAt}
         />
         <Body content={board?.content} />
-      </>
+      </S.container>
 
       {comments ? <Comment replies={comments} /> : <div>댓글이 없습니다.</div>}
+      <CommentWrite boardId={boardId} />
     </div>
   );
 };
 
 export default BoardDetail;
+
+const S = {
+  container: styled.div`
+    border: 2px solid #000;
+    border-radius: 5px;
+    min-width: 800px;
+  `,
+  titleContainer: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 2rem;
+    border-bottom: 2px solid #000;
+  `,
+  title: styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+  `,
+  deleteOrUpdateButton: styled.div`
+    margin-right: 2rem;
+  `,
+};
--- my-app/src/pages/BoardMain.jsx
+++ my-app/src/pages/BoardMain.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
+import queryKeys from "queries/queryKeys";
 import styled from "styled-components";
+import * as S from "../../../admin/dev5/styles/styled";
 import { useGetBoards } from "pages/admin/dev5/queries";
 import BoardList from "../components/BoardList";
 import BoardEmpty from "../components/BoardEmpty";
-
+import usePaginationStore from "../../../admin/dev5/store/pagenationStore";
+import Pagination from "../../../admin/dev5/dev5components/Pagination";
+import DropDown from "../components/DropDown";
 const BoardMain = () => {
-  const { data, error, isLoading } = useGetBoards();
+  const queryClient = useQueryClient();
+  const { currentPage, searchQuery, setSearchQuery, order, setCurrentPage } =
+    usePaginationStore();
+  const { data, error, isLoading } = useGetBoards({
+    take: 10,
+    page: currentPage,
+    order: "ASC",
+    search: searchQuery,
+  });
   // const [boards, setBoards] = useState();
-
+  const [search, setSearch] = useState("");
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -17,30 +30,68 @@ const BoardMain = () => {
   if (error) {
     return <div>{error.message}</div>;
   }
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const onKeyPressSearch = (e) => {
+    if (e.key === "Enter") {
+      setSearchQuery(search);
+      setSearch("");
+    }
+  };
+  const onClickSearch = () => {
+    if (!search) return alert("검색어를 입력해주세요");
+    setSearchQuery(search);
+    setSearch("");
+  };
+  const onClickReset = () => {
+    setCurrentPage(1);
+    setSearchQuery("");
+    queryClient.invalidateQueries([queryKeys.ADMIN, "boards"]);
+  };
   return (
-    <>
-      <S.header>
+    <Sub.container>
+      <Sub.header>
         <h1>게시판</h1>
-        <h3>임시 카테고리</h3>
-      </S.header>
-
-      <S.boardHeader>
-        <div>번호</div>
-        <div>제목</div>
-        <div>작성자</div>
-        <div>작성일</div>
-        <div>조회수</div>
-        <div>좋아요</div>
-      </S.boardHeader>
-      {/* {data?.length !== 0 ? <BoardList boards={data} /> : <BoardEmpty />} */}
-      <BoardList boards={data} />
-    </>
+        <S.Search>
+          {searchQuery ? <span>현재검색어:{searchQuery} </span> : null}
+          <S.SearchInput
+            placeholder="검색어를 입력하세요"
+            value={search}
+            onChange={onChangeSearch}
+            onKeyDown={onKeyPressSearch}
+          />
+          <S.SearchButton onClick={onClickSearch}>검색</S.SearchButton>
+          <S.ResetButton onClick={onClickReset}>초기화</S.ResetButton>
+        </S.Search>
+        <DropDown />
+      </Sub.header>
+      <S.Container>
+        <Sub.boardHeader>
+          <div>번호</div>
+          <div>제목</div>
+          <div>작성자</div>
+          <div>작성일</div>
+          <div>조회수</div>
+          <div>좋아요</div>
+        </Sub.boardHeader>
+        {/* {data?.length !== 0 ? <BoardList boards={data} /> : <BoardEmpty />} */}
+        <BoardList boards={data} />
+        <Sub.paginationContainer>
+          <Pagination />
+        </Sub.paginationContainer>
+      </S.Container>
+    </Sub.container>
   );
 };
 
 export default BoardMain;
 
-const S = {
+const Sub = {
+  container: styled.div`
+    width: 100%;
+    height: 100%;
+  `,
   header: styled.div`
     display: flex;
     justify-content: space-between;
@@ -48,8 +99,18 @@ const S = {
   boardHeader: styled.div`
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 4fr 0.5fr 0.5fr 0.5fr 0.5fr;
+    grid-template-columns: 1fr 4fr 0.5fr 1fr 0.5fr 0.5fr;
     padding: 1rem;
     border-bottom: 1px solid #000;
+  `,
+  paginationContainer: styled.div`
+    width: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 30px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
   `,
 };
