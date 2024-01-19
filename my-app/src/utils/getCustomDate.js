88a340746341c9010409627c46efd0b7f39e871e
jangco97 <qkrnslehd@naver.com>
2024-01-19 23:20:07 +0900

    feat: 커스텀 데이트 변환 함수

---
+++ my-app/src/utils/getCustomDate.js
@@ -0,0 +1,10 @@
+export const getCustomData = (date) => {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const day = newDate.getDate();
+
+  const customDate = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+
+  return customDate;
+};
