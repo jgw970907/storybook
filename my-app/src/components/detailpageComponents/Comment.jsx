505b56ed93a7cbd265af43319a23dfbe2b896433
jangco97 <qkrnslehd@naver.com>
2024-01-21 23:48:49 +0900

    feat: 댓글 수정 및 삭제 유저한테만 보여주기

--- my-app/src/components/detailpageComponents/Comment.jsx
+++ my-app/src/components/detailpageComponents/Comment.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-
-const Comment = ({ replies }) => {
+import React from "react";
+import * as S from "../../../../admin/dev5/styles/detailPageStyled";
+const Comment = ({ replies, user }) => {
   console.log(replies);
   return (
-    <div>
-      {replies.map((reply) => {
+    <S.CommentContainer>
+      {replies.map((reply, index) => {
         return (
-          <div key={reply.id}>
-            <div>{reply.author.nickname}</div>
-            <div>{reply.reply5}</div>
-            <div>{reply.createdAt}</div>
-          </div>
+          <S.CommentItemContainer key={reply.id} $index={index}>
+            <div>
+              <span>작성자:{reply.author.nickname}</span>
+              <span>
+                <div>{reply.reply5}</div>
+              </span>
+            </div>
+
+            <S.CommentItemRight>
+              <span>{reply.createdAt}</span>
+              {user?.nickname === reply?.author?.nickname && (
+                <S.CommentButtonContainer>
+                  <S.CommentButton>수정</S.CommentButton>
+                  <S.CommentButton>삭제</S.CommentButton>
+                </S.CommentButtonContainer>
+              )}
+            </S.CommentItemRight>
+          </S.CommentItemContainer>
         );
       })}
-    </div>
+    </S.CommentContainer>
   );
 };
 
