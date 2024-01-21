3985a0e3d7f5ee7d047f8bfc6b041a5f9cc0f935
jangco97 <qkrnslehd@naver.com>
2024-01-21 23:49:44 +0900

    refactor: 버튼 및 인풋 에러 핸들링 가능하게 커스텀

--- my-app/src/components/UIElements/Button.tsx
+++ my-app/src/components/UIElements/Button.tsx
@@ -1,12 +1,34 @@
-import ButtonType from "../../types/UIElementType/ButtonType";
-const Button = ({ label, onClick, disabled }: ButtonType) => {
+import React from "react";
+import styled from "styled-components";
+const Button = ({ content, onClick, size, disabled }) => {
   return (
-    <>
-      <button onClick={onClick} disabled={disabled}>
-        {label}
-      </button>
-    </>
+    <S.button onClick={onClick} disabled={disabled} $size={size}>
+      {content}
+    </S.button>
   );
 };
 
 export default Button;
+
+const S = {
+  button: styled.button`
+    width: ${(props) => props.$size || "auto"};
+    height: 50px;
+    background-color: ${({ theme, disabled }) =>
+      disabled ? theme.colors.disabled : theme.colors.primary};
+    color: ${({ theme }) => theme.colors.G_200};
+    border: none;
+    border-radius: 5px;
+    cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+    transition: background-color 0.3s, transform 0.1s;
+
+    &:hover {
+      background-color: ${({ theme, disabled }) =>
+        disabled ? theme.colors.disabled : theme.colors.primaryHover};
+    }
+
+    &:active {
+      transform: ${({ disabled }) => (disabled ? "none" : "scale(0.98)")};
+    }
+  `,
+};
