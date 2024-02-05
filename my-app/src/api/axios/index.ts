1c0100c6b7740fe0784c729595c060cca08b206f
jangco97 <qkrnslehd@naver.com>
2024-02-05 12:07:30 +0900

    feat: axios 모듈화

--- my-app/src/api/axios/index.ts
+++ my-app/src/api/axios/index.ts
@@ -1,37 +1,51 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import injectInterceptors from "./interceptors";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const instance = injectInterceptors(axios.create({ baseURL: BASE_URL }));
 
-const createAxiosInstance = (url: string) => {
-  const endpoint = url;
-  const instance: AxiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-  });
-
-  const get = async (params: object = {}) => {
+export const getAxiosInstance = (endpoint: string) => {
+  const get = async <T>(queries: object = {}) => {
     try {
-      const res = await instance.get(endpoint, params);
-
+      const res = await instance.get<T>(endpoint, {
+        params: queries,
+      } as AxiosRequestConfig);
       const { data, status } = res;
       return { ...data, status };
     } catch (error) {
       console.error(error);
     }
   };
-
-  const post = async (params: object = {}, config?: AxiosRequestConfig) => {
+  const post = async <T>(params: object = {}, config?: AxiosRequestConfig) => {
     try {
-      const res = await instance.post(endpoint, params, config);
-
+      const res = await instance.post<T>(endpoint, params, config);
       const { data, status } = res;
       return { ...data, status };
     } catch (error) {
       console.error(error);
     }
   };
-
-  return { get, post };
+  const patch = async <T>(params: object = {}, config?: AxiosRequestConfig) => {
+    try {
+      const res = await instance.patch<T>(endpoint, params, config);
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const remove = async <T>(params: object = {}) => {
+    try {
+      const res = await instance.delete<T>(endpoint, {
+        data: params,
+      });
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return { get, post, patch, remove };
 };
 
-export default createAxiosInstance;
+export default getAxiosInstance;
