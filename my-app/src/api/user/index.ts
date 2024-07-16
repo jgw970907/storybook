343a4fdb3c08f8762f83fc0e7360166dcc34a66f
jangco97 <qkrnslehd@naver.com>
2024-07-16 21:10:04 +0900

    /api 제거

--- my-app/src/api/user/index.ts
+++ my-app/src/api/user/index.ts
@@ -8,18 +8,18 @@ type Response = {
 
 // 유저 목록
 export const getUserlist = async () => {
-  const res = await Axios(`/api/users`).get<Response>();
+  const res = await Axios(`/users`).get<Response>();
 
   return res;
 };
 
 export const deleteUser = async (id: string | string) => {
-  const res = await Axios(`/api/users/delete/${id}`).get<Response>();
+  const res = await Axios(`/users/delete/${id}`).get<Response>();
 
   return res;
 };
 export const patchUser = async (params: UserPatchReq) => {
-  const res = await Axios(`/api/users/update`).patch(params);
+  const res = await Axios(`/users/update`).patch(params);
 
   return res;
 };
