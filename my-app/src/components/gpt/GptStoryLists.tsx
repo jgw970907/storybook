8f4dfa738a643b65e38f09405adcb5145ae48492
jangco97 <qkrnslehd@naver.com>
2024-09-13 16:38:11 +0900

    add component for listing bestStories and randomStories

---
+++ my-app/src/components/gpt/GptStoryLists.tsx
@@ -0,0 +1,60 @@
+import { CardSlide } from 'components/shared/CardSlide';
+import styled from 'styled-components';
+import { getStyledColor } from 'utils';
+import { MdRecommend } from 'react-icons/md';
+import { FaRegSmile } from 'react-icons/fa';
+import Spinner from 'components/shared/Spinner';
+import { useGetBestStories, useGetRandomStories } from 'queries/gpt';
+const SectionTitle = styled.h2`
+  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3rem;
+  width: 10rem;
+  padding: 0.5rem 1rem;
+  font-size: 1.5rem;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  border-radius: 1rem;
+  color: ${getStyledColor('forest', 700)};
+  background-color: ${getStyledColor('forest', 300)};
+`;
+const SpinnerWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10rem;
+`;
+export const GptStoryLists = () => {
+  const { data: bestStories, status: bestloading } = useGetBestStories();
+  const { data: randomStories, status: recommendloading } = useGetRandomStories();
+  return (
+    <div>
+      <SectionTitle>
+        최고작
+        <FaRegSmile />
+      </SectionTitle>
+      {bestloading === 'loading' ? (
+        <SpinnerWrap>
+          <Spinner width="3rem" />{' '}
+        </SpinnerWrap>
+      ) : (
+        <CardSlide items={bestStories?.data.stories} />
+      )}
+
+      <SectionTitle>
+        추천작
+        <MdRecommend />
+      </SectionTitle>
+      {recommendloading === 'loading' ? (
+        <SpinnerWrap>
+          <Spinner width="3rem" />{' '}
+        </SpinnerWrap>
+      ) : (
+        <CardSlide items={randomStories?.data.stories} />
+      )}
+    </div>
+  );
+};
