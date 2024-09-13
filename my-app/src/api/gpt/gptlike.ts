b522b10d2d067e1c536f420f06a7ff67d2041f93
jangco97 <qkrnslehd@naver.com>
2024-09-13 16:30:43 +0900

    add api for gptlike

---
+++ my-app/src/api/gpt/gptlike.ts
@@ -0,0 +1,27 @@
+import { MyFavoritesParams } from 'types/likeTypes';
+import { getAxiosInstance as Axios } from '../axios/index';
+
+import { StoryisLikeRes, StoryChangeLikeRes, MyFavorites } from 'types/gptTypes';
+
+export const getStoryLike = async ({ storyId, userId }: { storyId: string; userId: string }) => {
+  const res = await Axios(`/gptlike/${storyId}/${userId}`).get<StoryisLikeRes>();
+  return res;
+};
+
+export const addLike = async ({ storyId, userId }: { storyId: string; userId: string }) => {
+  if (!userId) return;
+  const res = await Axios(`/gptlike/add/${storyId}/${userId}`).patch<StoryChangeLikeRes>();
+  return res;
+};
+export const removeLike = async ({ storyId, userId }: { storyId: string; userId: string }) => {
+  if (!userId) return;
+  const res = await Axios(`/gptlike/remove/${storyId}/${userId}`).patch<StoryChangeLikeRes>();
+  return res;
+};
+export const getMyFavoriteStories = async (params: MyFavoritesParams) => {
+  const { take, page } = params;
+  const res = await Axios(
+    `/myfavorites?take=${take}&page=${page}&order__createdAt=DESC`,
+  ).get<MyFavorites>();
+  return res;
+};
